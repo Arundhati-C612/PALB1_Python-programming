336044a8c6c3077d1a2cd2ea05f4ff32a68ca948
Arundhati Chauhan <arundhatic612@gmail.com>
2026-03-05 08:51:30 +0530

    Jump Game2_leetcode.py

---
+++ Jump Game2_leetcode.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def jump(self, nums):
+        n = len(nums)
+        jumps = 0
+        current_end = 0
+        farthest = 0
+
+        for i in range(n - 1):  # we don't need to jump from the last index
+            farthest = max(farthest, i + nums[i])
+
+            if i == current_end:
+                jumps += 1
+                current_end = farthest
+
+        return jumps
