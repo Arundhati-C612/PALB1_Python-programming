6de8fe1ee5a7a72f77ea5156dfb36b7d90092e6a
Arundhati Chauhan <arundhatic612@gmail.com>
2026-03-05 08:35:27 +0530

    First&Last PositionOfElement _leetcode.py

---
+++ First&Last PositionOfElement _leetcode.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        
+        def findBound(isFirst):
+            left, right = 0, len(nums) - 1
+            bound = -1
+            
+            while left <= right:
+                mid = (left + right) // 2
+                
+                if nums[mid] == target:
+                    bound = mid
+                    if isFirst:
+                        right = mid - 1   # move left
+                    else:
+                        left = mid + 1    # move right
+                elif nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
+            return bound
+        
+        first = findBound(True)
+        if first == -1:
+            return [-1, -1]
+        
+        last = findBound(False)
+        return [first, last]
